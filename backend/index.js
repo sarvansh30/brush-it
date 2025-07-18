d29ad2b260b8a8b79ff0acd968c871eb97e17cd4
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-07-19 04:05:07 +0530

    Initial websocket connection done

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const http = require('http');
+const {WebSocketServer} = require('ws');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
 
+const wss = new WebSocketServer({server});
 
-app.get('/',(req,res)=>{
-    res.send('hi');
+wss.on('connection',(ws)=>{
+    console.log("new client connected");
+
+    ws.on('message',(message)=>{
+        wss.clients.forEach(client =>{
+        if(client !==ws && client.readyState === WebSocket.OPEN){
+            client.send(message);
+        }
+    });
+    });
+
+    ws.on('close',()=>{
+        console.log("client disconnected");
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+server.listen(PORT,()=>console.log(`Server listening on port: ${PORT}`));
