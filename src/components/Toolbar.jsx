90a2dbcfe0996588a8c66c20ba54a6203395fa99
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-08-04 15:37:41 +0530

    Added proper functionality for ERASE

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,16 +4,11 @@ import React, { useContext } from 'react';
 const ToolBar = () =>{
     const {tool,setTool,color,setColor,strokeWidth,setStrokeWidth} = useContext(ToolContext);
 
-    const handleClick = ()=>{
-        setColor("#FFFFFF");
-        setTool("ERASE");
-    }
-
     return(
         <div className="fixed bottom-16 left-1/2 -translate-x-1/2  p-2 rounded-xl border-1 flex gap-7">
 
-        <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={()=>setColor('#000000')}>Draw</button>
-        <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={handleClick}>Erase</button>
+        <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={()=>setTool('DRAW')}>Draw</button>
+        <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={()=>setTool("ERASE")}>Erase</button>
 
         </div>
     );
