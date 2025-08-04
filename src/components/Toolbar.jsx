1363d7ce3f7d194440d5af970eeb7fbf35323d54
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-08-04 16:27:27 +0530

    added color picker

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -8,7 +8,9 @@ const ToolBar = () =>{
         <div className="fixed bottom-16 left-1/2 -translate-x-1/2  p-2 rounded-xl border-1 flex gap-7">
 
         <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={()=>setTool('DRAW')}>Draw</button>
+        
         <button className="hover:bg-amber-200 hover:cursor-pointer" onClick={()=>setTool("ERASE")}>Erase</button>
+        
         <p>Stroke: </p>
         <input 
         type="range" 
@@ -20,6 +22,15 @@ const ToolBar = () =>{
         defaultValue={5}
         onChange={(e)=>setStrokeWidth(parseInt(e.target.value,10))} />
 
+        <p>Color:</p>
+        <input 
+        type="color"
+         name="color"
+          id="color"
+          defaultValue={"black"}
+          value={color}
+          onChange={(e)=>setColor(e.target.value)} />
+        
         </div>
     );
 };
