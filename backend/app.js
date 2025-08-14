2c616d49053259658ebbef693634f6e472af79d9
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-08-15 00:45:15 +0530

    fixed some bugs for room endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const {v4  :uuidv4} = require('uuid');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 
 app.get('/status',(req,res)=>{
     res.send("Server is up and runnning");
 });
 
-app.post('/create-room',(req,res)=>{
-    const roomId = uuidv4();
-
+app.post('/room/create-room',(req,res)=>{
+    const roomid = uuidv4();
+    console.log("Room created with ID:", roomid);
     res.json({
-        roomId: roomId,
+        roomid: roomid,
         message: "Room created successfully"
     });
 });
 
 app.get('/room/:roomId',(req,res)=>{
     const roomId = req.params.roomId;
-    
+
 });
 
 module.exports =app;
